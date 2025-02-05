333706cc2d7ac364f87c3166dfc6fab01b13523a
王宇逸 <Strawberry_Str@hotmail.com>
2025-02-05 10:03:33 +0800

    Correct metadata

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-extern crate duckdb;
-extern crate duckdb_loadable_macros;
-extern crate libduckdb_sys;
-
 use duckdb::{
     core::{DataChunkHandle, Inserter, LogicalTypeHandle, LogicalTypeId},
     vtab::{BindInfo, Free, FunctionInfo, InitInfo, VTab},
@@ -43,7 +39,10 @@ impl VTab for HelloVTab {
     type InitData = HelloInitData;
     type BindData = HelloBindData;
 
-    unsafe fn bind(bind: &BindInfo, data: *mut HelloBindData) -> Result<(), Box<dyn std::error::Error>> {
+    unsafe fn bind(
+        bind: &BindInfo,
+        data: *mut HelloBindData,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         bind.add_result_column("value", LogicalTypeHandle::from(LogicalTypeId::Varchar));
         let param = bind.get_parameter(0).to_string();
         unsafe {
@@ -52,14 +51,20 @@ impl VTab for HelloVTab {
         Ok(())
     }
 
-    unsafe fn init(_: &InitInfo, data: *mut HelloInitData) -> Result<(), Box<dyn std::error::Error>> {
+    unsafe fn init(
+        _: &InitInfo,
+        data: *mut HelloInitData,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         unsafe {
             (*data).done = false;
         }
         Ok(())
     }
 
-    unsafe fn func(func: &FunctionInfo, output: &mut DataChunkHandle) -> Result<(), Box<dyn std::error::Error>> {
+    unsafe fn func(
+        func: &FunctionInfo,
+        output: &mut DataChunkHandle,
+    ) -> Result<(), Box<dyn std::error::Error>> {
         let init_info = func.get_init_data::<HelloInitData>();
         let bind_info = func.get_bind_data::<HelloBindData>();
 
@@ -70,7 +75,7 @@ impl VTab for HelloVTab {
                 (*init_info).done = true;
                 let vector = output.flat_vector(0);
                 let name = CString::from_raw((*bind_info).name);
-                let result = CString::new(format!("Rusty Quack {} 🐥", name.to_str()?))?;
+                let result = CString::new(format!("Rusty Quack {}", name.to_str()?))?;
                 // Can't consume the CString
                 (*bind_info).name = CString::into_raw(name);
                 vector.insert(0, result);
@@ -85,11 +90,8 @@ impl VTab for HelloVTab {
     }
 }
 
-const EXTENSION_NAME: &str = env!("CARGO_PKG_NAME");
-
-#[duckdb_entrypoint_c_api(ext_name = "rusty_quack", min_duckdb_version = "v0.0.1")]
-pub unsafe fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
-    con.register_table_function::<HelloVTab>(EXTENSION_NAME)
-        .expect("Failed to register hello table function");
+#[duckdb_entrypoint_c_api(ext_name = "hdf5", min_duckdb_version = "v0.0.1")]
+pub fn extension_entrypoint(con: Connection) -> Result<(), Box<dyn Error>> {
+    con.register_table_function::<HelloVTab>("hdf5_read")?;
     Ok(())
 }
